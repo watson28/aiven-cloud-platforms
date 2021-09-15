b15d579a5f289f7e527ec1aa8effbd86e00b2fff
Jorge Florian <jorge.florian@sennder.com>
2021-09-15 10:38:57 +0200

    add ui component library, linting, code formatting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
 
 function App() {
   return (
     <div className="App">
+      <Typography variant="h1">
+        Aiven cloud selection
+      </Typography>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
