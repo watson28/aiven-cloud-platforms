be8ea5ccc92d6b5fce2ec6f499c2fdafeb11238d
Jorge Florian <jorge.florian@sennder.com>
2021-09-21 10:22:52 +0200

    cache cloud platforms on repository

--- backend/src/cloud_platforms_repository.py
+++ backend/src/cloud_platforms_repository.py
@@ -1,15 +1,35 @@
-from typing import Sequence
+import json
+import os
+import logging
+from json.decoder import JSONDecodeError
+from typing import Sequence, Optional
 from src.cloud_platforms_model import CloudPlatform
+from pathlib import Path
 
+class CloudPlatformNotFoundException(BaseException):
+	pass
 
 class CloudPlatformsRepository:
-	platforms: Sequence[CloudPlatform]
+	_CACHE_FILE_PATH = os.path.join(os.getcwd(), '.cache/cloud_platforms')
+	platforms: Optional[Sequence[CloudPlatform]]
 
 	def __init__(self) -> None:
-		self.platforms = []
+		self.platforms = None
+		try:
+			Path(os.path.basename(self._CACHE_FILE_PATH)).mkdir(exist_ok=True)
+			with open(self._CACHE_FILE_PATH) as cache_file:
+				self.platforms = [CloudPlatform(**p) for p in json.load(cache_file)]
+		except JSONDecodeError:
+			logging.exception('Failed to load cloud platform cache')
+		except FileNotFoundError:
+			logging.info('Cache file not found')
 	
 	def save_cloud_platforms(self, platforms: Sequence[CloudPlatform]):
 		self.platforms = platforms
+		with open(self._CACHE_FILE_PATH, 'w') as file:
+			json.dump([p.dict() for p in platforms], file)
 
 	def get_cloud_platforms(self) -> Sequence[CloudPlatform]:
+		if self.platforms is None:
+			raise CloudPlatformNotFoundException()
 		return self.platforms
